3b9165f9cb0215dca7667385e0c944ed427c281d
dasdom <dom@dasdom.de>
2015-04-12 12:12:43 +0200

    Initial Commit

---
+++ Poster/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Poster
+//
+//  Created by dasdom on 12.04.15.
+//  Copyright (c) 2015 Dominik Hauser. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Poster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Poster
+//
+//  Created by dasdom on 12.04.15.
+//  Copyright (c) 2015 Dominik Hauser. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
